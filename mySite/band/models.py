434f502f19601795328d1a982b8ed076870db420
Werner <muismiller@gmail.com>
2023-11-13 12:05:15 +0200

    Band docstrings added

--- mySite/band/models.py
+++ mySite/band/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 
 # Class to allow the admin to add albums to the database.
 class Album(models.Model):
+    """Model representing an album in the database.
+
+    :param models.Model: The base class for all Django database models.
+    :type models.Model: class
+    :ivar title: The title of the album.
+    :vartype title: str
+    :ivar release_date: The release date of the album.
+    :vartype release_date: DateField
+    :ivar description: A description of the album.
+    :vartype description: TextField
+    :return: A string representation of the album.
+    :rtype: str
+    """    
     title = models.CharField(max_length=100)
     release_date = models.DateField()
     description = models.TextField()
@@ -14,6 +27,19 @@ class Album(models.Model):
 
 # Class to allow the admin to add live show details to the database.
 class LiveShow(models.Model):
+    """Model representing live show details in the database.
+
+    :param models.Model: The base class for all Django database models.
+    :type models.Model: class
+    :ivar date: The date of the live show.
+    :vartype date: DateField
+    :ivar venue: The venue where the live show takes place.
+    :vartype venue: CharField
+    :ivar location: The location of the live show.
+    :vartype location: CharField
+    :return: A string representation of the live show.
+    :rtype: str
+    """    
     date = models.DateField()
     venue = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
--- mySite/band/views.py
+++ mySite/band/views.py
@@ -4,25 +4,44 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Album, LiveShow
 
 
-# Function for the landing page.
-#def landing_page(request):
-#    return render(request, 'landing_page.html')
-
-
 # Function for the album page.
 def album_page(request):
+    """Render the album page.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: Rendered HTML response for the album page.
+    :rtype: HttpResponse
+    """    
     albums = Album.objects.all()
     return render(request, 'album_page.html', {'albums': albums})
 
 
 # Function for the live show details page.
 def live_shows_page(request):
+    """Render the live shows page.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: Rendered HTML response for the live shows page.
+    :rtype: HttpResponse
+    """    
     live_shows = LiveShow.objects.all()
     return render(request, 'live_shows_page.html', {'live_shows': live_shows})
 
 
 # Function for the landing page.
 def landing_page(request):
+    """Render the landing page.
+
+    If the request method is POST, attempt to authenticate the user.
+    If authentication is successful, log in the user and redirect to the landing page.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: Rendered HTML response for the landing page.
+    :rtype: HttpResponse
+    """
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -35,6 +54,16 @@ def landing_page(request):
 
 # Function to register.
 def register(request):
+    """Handle user registration.
+
+    If the request method is POST, validate the registration form.
+    If the form is valid, create a new user, log in, and redirect to the landing page.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: Rendered HTML response for the registration page.
+    :rtype: HttpResponse
+    """    
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -48,5 +77,12 @@ def register(request):
 
 # Function for the logging out.
 def logout_view(request):
+    """Log out the user.
+
+    :param request: The HTTP request.
+    :type request: HttpRequest
+    :return: Redirect to the landing page after logging out.
+    :rtype: HttpResponse
+    """    
     logout(request)
     return redirect('landing_page')
